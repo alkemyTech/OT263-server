c755e92471a5c7d092e94c2cc8058339851ff3e3
Juan Manuel Maroñas <jmmaronas@gmail.com>
2022-08-22 19:05:45 -0300

    fixed

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,21 +1,7 @@
-const sequelize=require('../db')
-const {DataTypes} =require('sequelize')
-const  User  = require('../models/user')(sequelize, DataTypes)
-
-async function connectDB() {
-    try {
-        await sequelize.sync({ force: false });
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
-
-connectDB()
-
+const  db  = require('../models')
 
 const createUser = async (user) => {
-    const newUser = await User.create(user)
+    const newUser = await db.User.create(user)
     console.log(newUser)
     return newUser.toJSON()
 }
