e49c37d65a9798e2eea003d8f2d65b1655bfeca2
Fuku <fukushimafederico@hotmail.com>
2022-08-21 21:47:02 -0300

    Endpoint datos públicos Organización: Create get(organizations) request for testimonies

---
+++ routes/organizations.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const {Organizations} = require('../models')
+
+/* GET organizations listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/1/public', async function(req, res) {
+  const listOfOrganizations = await Organizations.findAll();
+  const {name, image, phone, address, welcomeText} = listOfOrganizations;
+  res.json({
+    name: name,
+    image: image,
+    phone: phone,
+    address: address,
+    welcomeText: welcomeText,
+  })
+})
+
+module.exports = router;
