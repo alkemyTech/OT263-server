ad6b4fc8e33bb46340d374e5e6cb48c45714c18e
Sebastian Garcia Nallar <sebagn@hotmail.com>
2022-08-23 14:11:33 -0300

    password as an env varible

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,26 +1,24 @@
 const nodemailer = require("nodemailer");
 
-async function sendMail(to, subject, text) {
-    // create reusable transporter object using the default SMTP transport
+async function sendEmail(to, subject, text) {
+
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 465,
         secure: true, // true for 465, false for other ports
         auth: {
             user: "ongsomosmas.demo@gmail.com",
-            pass: "kaohxjkciqackpdj",
+            pass: process.env.NODEMAILER_API_KEY,
         },
     });
 
-    // send mail with defined transport object
     let info = await transporter.sendMail({
-        from: "ongsomosmas.demo@gmail.com", // sender address
+        from: "ongsomosmas.demo@gmail.com", 
         to: to, // list of receivers
-        subject: subject, // Subject line
-        text: text, // plain text body
+        subject: subject, 
+        text: text, 
     });
 
-    console.log("Message sent: %s", info.messageId);
 }
 
-module.exports = sendMail;
+module.exports = sendEmail;
