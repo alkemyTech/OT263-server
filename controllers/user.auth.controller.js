c00cf3e83fda94d3719a5b1df6d38884df571147
Fuku <fukushimafederico@hotmail.com>
2022-08-25 00:34:29 -0300

    update controller

---
+++ controllers/user.auth.controller.js
@@ -0,0 +1,27 @@
+const { User } = require('../models');
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+
+
+const loginUser = async function(req, res) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {email, password} = req.body;
+  const user = await User.findOne({ where: {email: email} });
+
+  if(!user){
+    return res.json({error: "User Doesn't Exist", ok: false})
+  } else {
+    bcrypt.compare(password, user.password).then((match) => {
+    if(!match){
+      return res.json({error: "Wrong Username And Password Combination", ok: false});
+    }
+    return res.json(user)
+    })
+  }
+}
+
+module.exports = loginUser;
