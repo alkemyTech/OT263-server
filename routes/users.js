7ebcfb3595ec51f34c1ce8fa4224e5489b78350b
Juan Manuel Maroñas <jmmaronas@gmail.com>
2022-08-25 10:38:11 -0300

    change apllied in router/user.js

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,30 @@
-const express = require('express');
-const router = express.Router();
+var express = require("express");
+var router = express.Router();
+const { requireAuth } = require("../middlewares/requireAuth");
 const createUser = require('../controllers/user.auth.controller')
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-})
+const UserController = require("../controllers/user.controller");
+
+const controller = new UserController();
+
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get(
+  "/auth/me",
+  requireAuth,
+  async function (req, res, next) {
+    try {
+      const userId = req.user.sub;
+      const user = await controller.getUserById(userId);
+      res.send(user);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
 router.post('/auth/register', createUser)
 
 module.exports = router;
