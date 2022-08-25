c4898afa4caad3c9840f6404c6ff567252911239
Juan Manuel Maroñas <jmmaronas@gmail.com>
2022-08-25 08:54:02 -0300

    fixed

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const createUser = require('../controllers/user.auth.controller')
 
 /* GET users listing. */
-usersRouter.get('/', function (req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
-});
+})
+router.post('/auth/register', createUser)
 
 module.exports = router;
