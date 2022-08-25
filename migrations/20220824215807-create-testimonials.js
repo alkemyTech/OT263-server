31259f5bcf772f199d005f56c75c1e099b60955d
Fuku <fukushimafederico@hotmail.com>
2022-08-25 00:16:20 -0300

    upload controllers

---
+++ migrations/20220824215807-create-testimonials.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: async  (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Testimonials', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.INTEGER.UNSIGNED
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      welcomeText: {
+        type: Sequelize.STRING
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Testimonials');
+  }
+};
