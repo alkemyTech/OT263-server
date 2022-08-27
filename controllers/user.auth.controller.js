f2621895e9ae082ec1b36ee1df66d96d72a1b3b6
Fuku <fukushimafederico@hotmail.com>
2022-08-27 18:26:02 -0300

    fx token

--- controllers/user.auth.controller.js
+++ controllers/user.auth.controller.js
@@ -20,8 +20,9 @@ const loginUser = async function(req, res) {
       if(!match){
         return res.json({error: "Wrong Username And Password Combination", ok: false});
       }
-    const payload = { sub: user.id, roleId: user.roleId }
+    const payload = { user: user.id, roleId: user.roleId }
     const token = createToken(payload)
+    console.log(token)
     return res.json( {token: token} )
     })
   }
