45b76526a4d2901ed5bdf080b6aae35e55bd7c57
Pablo Pastorino <phpastorino@outlook.com>
2022-08-27 16:22:36 -0300

    Adds delete route and controller for user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,17 +1,33 @@
-const { User } = require("../models/");
-const createError = require("http-errors");
+const { User } = require('../models/')
+const createError = require('http-errors')
 
 class UserController {
+	constructor() {}
 
-    constructor() {}
+	async getUserById(id) {
+		const user = await User.findByPk(id)
+		if (!user) {
+			throw createError.NotFound('User not found')
+		}
+		return user
+	}
 
-    async getUserById(id) {
-        const user = await User.findByPk(id);
-        if (!user) {
-            throw createError.NotFound("User not found");
-        }
-        return user;
-    }
+	async deleteUser(req, res) {
+		const { sub: id } = req.user
+
+		try {
+			if (id !== req.params.id) throw Error
+			else {
+				const row = await User.destroy({ where: { id } })
+
+				if (!row) return res.status(404).json(createError.NotFound())
+
+				return res.status(204)
+			}
+		} catch (error) {
+			return res.status(403).json(createError.Forbidden())
+		}
+	}
 }
 
-module.exports = UserController;
+module.exports = UserController
--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,36 @@
-var express = require("express");
-var router = express.Router();
-const { body } = require('express-validator');
-const loginUser = require('../controllers/user.auth.controller');
-const { requireAuth } = require("../middlewares/requireAuth");
-const UserController = require("../controllers/user.controller");
+var express = require('express')
+var router = express.Router()
+const { body } = require('express-validator')
+const loginUser = require('../controllers/user.auth.controller')
+const { requireAuth } = require('../middlewares/requireAuth')
+const UserController = require('../controllers/user.controller')
 
-const controller = new UserController();
+const controller = new UserController()
+
+router.use(requireAuth)
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', async function (req, res, next) {
+	res.send('respond with a resource')
+})
 
-router.post('/auth/login',
-  body('email').notEmpty().isEmail(),
-  body('password').isLength({ min: 5 }),
-  loginUser)
+router.post(
+	'/auth/login',
+	body('email').notEmpty().isEmail(),
+	body('password').isLength({ min: 5 }),
+	loginUser
+)
 
-router.get(
-    "/auth/me",
-    requireAuth,
-    async function (req, res, next) {
-        try {
-            const userId = req.user.sub;
-            const user = await  controller.getUserById(userId);
-            res.send(user);
-        } catch (err) {
-            next(err); 
-        }
-    }
-);
+router.delete('/:id', controller.deleteUser)
 
-module.exports = router;
+router.get('/auth/me', requireAuth, async function (req, res, next) {
+	try {
+		const userId = req.user.sub
+		const user = await controller.getUserById(userId)
+		res.send(user)
+	} catch (err) {
+		next(err)
+	}
+})
+
+module.exports = router
