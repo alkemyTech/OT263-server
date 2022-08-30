708ca037f2e7a150753145ddb7c572a81946ccb9
Pablo Pastorino <phpastorino@outlook.com>
2022-08-30 18:56:02 -0300

    Adds entries migration

--- models/entries.js
+++ models/entries.js
@@ -1,30 +1,31 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class Entries extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Entries.init({
-    name: DataTypes.STRING,
-    content: DataTypes.STRING,
-    image: DataTypes.STRING,
-    categoryId: DataTypes.NUMBER,
-    type: DataTypes.STRING,
-    deletedAt: DataTypes.DATE,
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'Entries',
-  });
-  return Entries;
-};
+	class Entries extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Entries.init(
+		{
+			name: DataTypes.STRING,
+			content: DataTypes.STRING,
+			image: DataTypes.STRING,
+			categoryId: DataTypes.NUMBER,
+			type: DataTypes.STRING,
+			deletedAt: DataTypes.DATE,
+			createdAt: DataTypes.DATE,
+			updatedAt: DataTypes.DATE
+		},
+		{
+			sequelize,
+			modelName: 'Entries'
+		}
+	)
+	return Entries
+}
