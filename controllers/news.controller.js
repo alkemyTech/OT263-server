1ca7c2836e4912b5fb79e6c6b04d273d9d0b0acb
Juan Manuel Maroñas <jmmaronas@gmail.com>
2022-09-02 00:50:48 -0300

    detail new.controller

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,9 +1,8 @@
-const db = require("../models")
+const { Entries } = require("../models")
 
 const getNewsById = async (req, res) => {
-    console.log(db.Entries)
     try {
-        const entriesNews = await db.Entries.findOne({ where: { id: req.params.id, type: "news" } })
+        const entriesNews = await Entries.findOne({ where: { id: req.params.id, type: "news" } })
         if(!entriesNews) throw new Error("The entry is not a news")
         return res.status(200).json(entriesNews)
     } catch (err) {
