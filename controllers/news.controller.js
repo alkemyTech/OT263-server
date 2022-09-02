d5c065900c622767da240e0971cbb21085633a26
Juan Manuel Maroñas <jmmaronas@gmail.com>
2022-09-01 21:44:40 -0300

    created endpoint /new/:is controller and route

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,8 +1,11 @@
-const { Entries } = require('../models')
+const db = require("../models")
 
 const getNewsById = async (req, res) => {
+    console.log(db.Entries)
     try {
-        return await Entries.findOne({where:{id:req.params.id, type:"news"}})
+        const entriesNews = await db.Entries.findOne({ where: { id: req.params.id, type: "news" } })
+        if(!entriesNews) throw new Error("The entry is not a news")
+        return res.status(200).json(entriesNews)
     } catch (err) {
         return res.status(404).json({ message: err.message })
     }
