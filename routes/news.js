92efa2ecd7b50bcd5d851de35c98708c61758ad5
Alan Coronado <contact@alancoronado.com>
2022-09-06 23:31:18 -0700

    feature/OT263-56 endpoint creacion novedades

--- routes/news.js
+++ routes/news.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getNewsById } = require('../controllers/news.controller')
+const { getNewsById, createNews } = require('../controllers/news.controller')
 
 router.get('/:id', getNewsById)
+router.post('/', createNews);
 
 module.exports = router
