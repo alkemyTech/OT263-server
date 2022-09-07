47df323ffa5f9a7b47080bddd6849f93385c7f86
Sebastian Garcia Nallar <sebagn@hotmail.com>
2022-09-07 11:00:30 -0300

    migration now returns a promise

--- migrations/20220906162734-testimonials-add-social-networks.js
+++ migrations/20220906162734-testimonials-add-social-networks.js
@@ -1,40 +1,47 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    return Promise.all([
-      queryInterface.addColumn(
-        'Testimonials', 
-        'Facebook', 
-        {
-          type: Sequelize.STRING,
-          allowNull: true,
-        },
-      ),
-      queryInterface.addColumn(
-        'Testimonials',
-        'Linkedin',
-        {
-          type: Sequelize.STRING,
-          allowNull: true,
-        },
-      ),
-      queryInterface.addColumn(
-        'Testimonials',
-        'Instagram',
-        {
-          type: Sequelize.TEXT,
-          allowNull: true,
-        },
-      ),
-    ]);
-  },
+    async up(queryInterface, Sequelize) {
+        return queryInterface.sequelize.transaction((t) => {
+            return Promise.all([
+                queryInterface.addColumn(
+                    "Testimonials",
+                    "facebook",
+                    {
+                        type: Sequelize.STRING,
+                        allowNull: true,
+                    },
+                    { transaction: t }
+                ),
+                queryInterface.addColumn(
+                    "Testimonials",
+                    "linkedin",
+                    {
+                        type: Sequelize.STRING,
+                        allowNull: true,
+                    },
+                    { transaction: t }
+                ),
+                queryInterface.addColumn(
+                    "Testimonials",
+                    "instagram",
+                    {
+                        type: Sequelize.STRING,
+                        allowNull: true,
+                    },
+                    { transaction: t }
+                ),
+            ]);
+        });
+    },
 
-  async down (queryInterface, Sequelize) {
-    return Promise.all([
-      queryInterface.removeColumn('Testimonials', 'Facebook'),
-      queryInterface.removeColumn('Testimonials', 'Linkedin'),
-      queryInterface.removeColumn('Testimonials', 'Instagram'),
-    ]);
-  }
+    async down(queryInterface, Sequelize) {
+        return queryInterface.sequelize.transaction((t) => {
+            return Promise.all([
+                queryInterface.removeColumn("Testimonials", "Facebook", { transaction: t }),
+                queryInterface.removeColumn("Testimonials", "Linkedin", { transaction: t }),
+                queryInterface.removeColumn("Testimonials", "Instagram", { transaction: t }),
+            ]);
+        });
+    },
 };
