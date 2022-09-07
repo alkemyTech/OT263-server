ca033aa61c8f53f6fa55d9b018f8fdacbd8b460f
Juan Manuel Maroñas <jmmaronas@gmail.com>
2022-09-07 08:47:38 -0300

    fixed destucturing

--- controllers/testimonials.controller.js
+++ controllers/testimonials.controller.js
@@ -1,7 +1,6 @@
 const { Testimonials } =require ('../models')
 
-const updateTestimonial=async (req, res)=>{
-    const { name, image, phone, address, welcomeText, deleteAt } = req.body
+const updateTestimonial=async (req, res)=>{    
     try{
         const testimonial= await Testimonials.update(req.body, {where:{id:req.params.id}})                
         if(testimonial[0])return res.status(200).json(req.body)
