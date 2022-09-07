c6ba4dc53b634d60b14caabbda26cfb19b1c2b77
Juan Manuel Maroñas <jmmaronas@gmail.com>
2022-09-07 08:46:44 -0300

    fixed console.log

--- controllers/testimonials.controller.js
+++ controllers/testimonials.controller.js
@@ -3,8 +3,7 @@ const { Testimonials } =require ('../models')
 const updateTestimonial=async (req, res)=>{
     const { name, image, phone, address, welcomeText, deleteAt } = req.body
     try{
-        const testimonial= await Testimonials.update(req.body, {where:{id:req.params.id}})        
-        console.log(testimonial)
+        const testimonial= await Testimonials.update(req.body, {where:{id:req.params.id}})                
         if(testimonial[0])return res.status(200).json(req.body)
         throw new Error ("Testimonial not found", 404)
     }catch(err){
