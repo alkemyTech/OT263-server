48643c6212311818b4439c4824c2cfe5cf7e0ea7
gabrielaNieves <gabrielapaez95@gmail.com>
2022-09-08 00:41:24 -0300

    Contact controller changes

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -3,9 +3,10 @@ const { Contacts } = require("../models")
 const getContacts = async (req, res) => {
     try {
         const contacts = await Contacts.findAll();
+        console.log(contacts)
         return res.status(200).json(contacts)
     } catch (err) {
-        return res.status(404).json(err);
+        return res.status(500).json(err);
     }
 }
 
