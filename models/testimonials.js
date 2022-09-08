627c0dde8027ddc8169cbde76fb146305c197fec
Alan Coronado <contact@alancoronado.com>
2022-09-07 18:17:04 -0700

    feature/OT263-82 model and migration testimonials

--- models/testimonials.js
+++ models/testimonials.js
@@ -1,27 +1,25 @@
 'use strict';
-const { Model } = require('sequelize');
-
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Testimonials extends Model {
+  class testimonials extends Model {
     /**
-    * Helper method for defining associations.
-    * This method is not a part of Sequelize lifecycle.
-    * The `models/index` file will call this method automatically.
-    */
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
       // define association here
     }
-  };
-  Testimonials.init({
+  }
+  testimonials.init({
     name: DataTypes.STRING,
     image: DataTypes.STRING,
-    phone: DataTypes.INTEGER.UNSIGNED,
-    address: DataTypes.STRING,
-    welcomeText: DataTypes.STRING,
-    deletedAt: DataTypes.DATE
+    content: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Testimonials',
+    modelName: 'testimonials',
   });
-  return Testimonials;
+  return testimonials;
 };
