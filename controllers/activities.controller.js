6225900ef018e1c865706ae102728442bdee80cd
gabrielaNieves <gabrielapaez95@gmail.com>
2022-09-08 01:55:12 -0300

    Create update activities endpoint

--- controllers/activities.controller.js
+++ controllers/activities.controller.js
@@ -2,11 +2,12 @@ const { Activities } = require("../models");
 
 const updateActivities = async (req, res) => {
     const {id} = req.params;
+	const activity = req.body;
 	try {
-		const updateActivity = await Activities.update(req.body, { where: { id } });
+		const [updateActivity] = await Activities.update(activity, { where: { id } });
 		if (!updateActivity) return res.status(404).json('No Activity found')
 
-		return res.status(200).json(updateActivity);
+		return res.status(200).json(activity);
 	} catch (error) {
 		return res.status(500).json(error)
 	}
