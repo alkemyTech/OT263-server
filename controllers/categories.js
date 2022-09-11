d45af375e0b40e7bfe7ec391f8044454ee2d12d8
Pablo Pastorino <phpastorino@outlook.com>
2022-09-10 21:04:29 -0300

    Creates POST /categories endpoint

---
+++ controllers/categories.js
@@ -0,0 +1,28 @@
+const { Category } = require('../models/')
+const createError = require('http-errors')
+const Joi = require('joi')
+
+const createCategory = async (req, res) => {
+	try {
+		const { error } = validateCategory(req.body)
+		if (error) return res.status(400).json(createError.BadRequest(error.details[0].message))
+
+		const category = await Category.create(req.body)
+
+		return res.status(200).json(category)
+	} catch (error) {
+		return res.status(500).json(createError.InternalServerError())
+	}
+}
+
+function validateCategory(entry) {
+	const schema = Joi.object({
+		name: Joi.string().required()
+	})
+
+	return schema.validate(entry)
+}
+
+module.exports = {
+	createCategory
+}
