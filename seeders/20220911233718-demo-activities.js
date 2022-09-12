16d2c55529de61598f7c7612ef1b1063862d2f96
Pablo Pastorino <phpastorino@outlook.com>
2022-09-11 22:16:51 -0300

    Creates activities seed boilerplate

---
+++ seeders/20220911233718-demo-activities.js
@@ -0,0 +1,44 @@
+'use strict'
+
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.bulkInsert(
+			'People',
+			[
+				{
+					id: {
+						allowNull: false,
+						autoIncrement: true,
+						primaryKey: true,
+						type: Sequelize.INTEGER
+					},
+					name: {
+						type: Sequelize.STRING
+					},
+					image: {
+						type: Sequelize.STRING
+					},
+					content: {
+						type: Sequelize.STRING
+					},
+					deletedAt: {
+						type: Sequelize.DATE
+					},
+					createdAt: {
+						allowNull: false,
+						type: Sequelize.DATE
+					},
+					updatedAt: {
+						allowNull: false,
+						type: Sequelize.DATE
+					}
+				}
+			],
+			{}
+		)
+	},
+
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete('People', null, {})
+	}
+}
