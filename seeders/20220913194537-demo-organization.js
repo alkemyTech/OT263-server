66845a0df30e6c4616a535864d5692e02f329bda
Pablo Pastorino <phpastorino@outlook.com>
2022-09-13 17:06:28 -0300

    Fixes organization missing fields

--- seeders/20220913194537-demo-organization.js
+++ seeders/20220913194537-demo-organization.js
@@ -3,18 +3,20 @@
 module.exports = {
 	async up(queryInterface, Sequelize) {
 		await queryInterface.bulkInsert(
-			'Organization',
+			'Organizations',
 			[
 				{
 					name: 'Somos Más',
 					image:
 						'https://images.unsplash.com/photo-1582213782179-e0d53f98f2ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-					phone: 54342155160972,
+					phone: '54123076',
 					address: 'Av. Andrés Rolón 1558, B1643BOP Béccar, Provincia de Buenos Aires',
 					welcomeText: 'Hola! Bienvenidx',
 					facebook: 'https://www.facebook.com/',
 					linkedin: 'https://www.linkedin.com/',
-					instagram: 'https://www.instagram.com/'
+					instagram: 'https://www.instagram.com/',
+					createdAt: new Date(),
+					updatedAt: new Date()
 				}
 			],
 			{}
@@ -22,6 +24,6 @@ module.exports = {
 	},
 
 	async down(queryInterface, Sequelize) {
-		await queryInterface.bulkDelete('Organization', null, {})
+		await queryInterface.bulkDelete('Organizations', null, {})
 	}
 }
