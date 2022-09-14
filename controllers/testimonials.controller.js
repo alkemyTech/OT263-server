0caaed1df4cc73df6f1828155042bdd5c4ec545d
Juan Manuel Maroñas <jmmaronas@gmail.com>
2022-09-14 10:12:54 -0300

    create endpoint delete testimonials

--- controllers/testimonials.controller.js
+++ controllers/testimonials.controller.js
@@ -1,8 +1,8 @@
-const { Testimonials } =require ('../models')
+const { testimonials } =require ('../models')
 
 const updateTestimonial=async (req, res)=>{    
     try{
-        const testimonial= await Testimonials.update(req.body, {where:{id:req.params.id}})                
+        const testimonial= await testimonials.update(req.body, {where:{id:req.params.id}})                
         if(testimonial[0])return res.status(200).json(req.body)
         throw new Error ("Testimonial not found", 404)
     }catch(err){
@@ -12,14 +12,15 @@ const updateTestimonial=async (req, res)=>{
 
 const deleteTestimonial=async (req,res)=>{
     try{
-        const result= await Testimonials.destroy({where:{id:req.body.id}})
-        console.log(result)
-        return res.json("eliminado")
+        const result= await testimonials.destroy({where:{id:req.params.id}})
+        if(!result)throw new Error("Elemento no encontrado", 404)
+        return res.status(200).json({message:"Elemento eliminado con exito"})
     }catch(err){
-        return
+        return res.status(404).json({message:err.message})
     }
 }
 
 module.exports = {
-    updateTestimonial
+    updateTestimonial,
+    deleteTestimonial
 }
--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
-const { updateTestimonial } = require('../controllers/testimonials.controller')
+const { updateTestimonial, deleteTestimonial } = require('../controllers/testimonials.controller')
+const { requireAuth } = require('../middlewares/requireAuth')
 const router = Router()
 
 router.put('/:id', updateTestimonial)
 
-router.delete('/:id',)
+router.delete('/:id', requireAuth, deleteTestimonial)
 
 module.exports = router
