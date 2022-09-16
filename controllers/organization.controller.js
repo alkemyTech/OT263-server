897d781adc3959f1270e0e4033416dc9ef97d3d9
Pablo Pastorino <phpastorino@outlook.com>
2022-09-16 10:21:32 -0300

    Adds multer test to handle images, aws-sdk and uuid dependencies

--- controllers/organization.controller.js
+++ controllers/organization.controller.js
@@ -1,38 +1,44 @@
-const { Organization } = require("../models");
+const { Organization } = require('../models')
 
 const createOrganization = async function (req, res) {
-    try {
-        const { name, image, phone, address, welcomeText } = req.body;
-        const newOrganization = await Organization.create({
-            name: name,
-            image: image,
-            phone: phone,
-            address: address,
-            welcomeText: welcomeText,
-        });
-        return res.json(newOrganization);
-    } catch (err) {
-        res.status(400).json(err);
-    }
-};
+	try {
+		const { name, image, phone, address, welcomeText } = req.body
+		const newOrganization = await Organization.create({
+			name: name,
+			image: image,
+			phone: phone,
+			address: address,
+			welcomeText: welcomeText
+		})
+		return res.json(newOrganization)
+	} catch (err) {
+		res.status(400).json(err)
+	}
+}
 
 const findOrganizationById = async function (req, res) {
-    const id = req.params.id;
-    const organizationData = await Organization.findByPk(id);
-    const { name, image, phone, address, welcomeText, facebook, linkedin, instagram } = organizationData;
-    res.json({
-        name: name,
-        image: image,
-        phone: phone,
-        address: address,
-        welcomeText: welcomeText,
-        facebook,
-        linkedin,
-        instagram
-    });
-};
+	const id = req.params.id
+	const organizationData = await Organization.findByPk(id)
+	const { name, image, phone, address, welcomeText, facebook, linkedin, instagram } =
+		organizationData
+	res.json({
+		name: name,
+		image: image,
+		phone: phone,
+		address: address,
+		welcomeText: welcomeText,
+		facebook,
+		linkedin,
+		instagram
+	})
+}
+const updateOrganization = (req, res) => {
+	console.log(req)
+	res.json(req.body.image)
+}
 
 module.exports = {
-    createOrganization,
-    findOrganizationById,
-};
+	createOrganization,
+	findOrganizationById,
+	updateOrganization
+}
--- routes/organizations.js
+++ routes/organizations.js
@@ -1,19 +1,23 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const multer = require('multer')
 const {
-  findOrganizationById, 
-  createOrganization
+	findOrganizationById,
+	createOrganization,
+	updateOrganization
 } = require('../controllers/organization.controller')
 
+const upload = multer({ dest: 'uploads/', name: 'image' })
 
 /* GET organizations listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-  
-});
+router.get('/', function (req, res, next) {
+	res.send('respond with a resource')
+})
+
+router.put('/:id', upload.single('image'), updateOrganization)
 
 router.get('/:id/public', findOrganizationById)
 
 router.post('/', createOrganization)
 
-module.exports = router;
+module.exports = router
