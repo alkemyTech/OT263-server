927e751354b30653f47ab2aa55792292b578886b
Pablo Pastorino <phpastorino@outlook.com>
2022-09-16 23:48:16 -0300

    Formatter and goes...

--- controllers/organization.controller.js
+++ controllers/organization.controller.js
@@ -1,39 +1,38 @@
-const { Organization } = require('../models')
+const { Organization } = require("../models");
 
 const createOrganization = async function (req, res) {
-	try {
-		const { name, image, phone, address, welcomeText } = req.body
-		const newOrganization = await Organization.create({
-			name: name,
-			image: image,
-			phone: phone,
-			address: address,
-			welcomeText: welcomeText
-		})
-		return res.json(newOrganization)
-	} catch (err) {
-		res.status(400).json(err)
-	}
-}
+    try {
+        const { name, image, phone, address, welcomeText } = req.body;
+        const newOrganization = await Organization.create({
+            name: name,
+            image: image,
+            phone: phone,
+            address: address,
+            welcomeText: welcomeText,
+        });
+        return res.json(newOrganization);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
 
 const findOrganizationById = async function (req, res) {
-	const id = req.params.id
-	const organizationData = await Organization.findByPk(id)
-	const { name, image, phone, address, welcomeText, facebook, linkedin, instagram } =
-		organizationData
-	res.json({
-		name: name,
-		image: image,
-		phone: phone,
-		address: address,
-		welcomeText: welcomeText,
-		facebook,
-		linkedin,
-		instagram
-	})
-}
+    const id = req.params.id;
+    const organizationData = await Organization.findByPk(id);
+    const { name, image, phone, address, welcomeText, facebook, linkedin, instagram } = organizationData;
+    res.json({
+        name: name,
+        image: image,
+        phone: phone,
+        address: address,
+        welcomeText: welcomeText,
+        facebook,
+        linkedin,
+        instagram
+    });
+};
 
 module.exports = {
-	createOrganization,
-	findOrganizationById
-}
+    createOrganization,
+    findOrganizationById,
+};
