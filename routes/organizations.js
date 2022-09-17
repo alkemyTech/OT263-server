ce1c207d076480f723c8f2cbcbff15c82951723a
Pablo Pastorino <phpastorino@outlook.com>
2022-09-16 23:02:59 -0300

    Fixes merging issues

--- routes/organizations.js
+++ routes/organizations.js
@@ -1,19 +1,21 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 const {
-	findOrganizationById,
-  createOrganization
+  findOrganizationById,
+	createOrganization
 } = require('../controllers/organization.controller')
+
 const handleStorage = require('../middlewares/handleStorage')
 const handleUpload = require('../middlewares/handleUpload')
 
 /* GET organizations listing. */
-router.get('/', function (req, res, next) {
-	res.send('respond with a resource')
-})
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+
+});
 
 router.get('/:id/public', findOrganizationById)
 
 router.post('/', createOrganization)
 
-module.exports = router
+module.exports = router;
