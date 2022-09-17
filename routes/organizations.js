45d6cf073c7a8145533cd17286fc92ff18a92039
Pablo Pastorino <phpastorino@outlook.com>
2022-09-16 23:15:50 -0300

    Another formatter fix

--- routes/organizations.js
+++ routes/organizations.js
@@ -1,17 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const {
-  findOrganizationById,
-	createOrganization
+  findOrganizationById, 
+  createOrganization
 } = require('../controllers/organization.controller')
 
-const handleStorage = require('../middlewares/handleStorage')
-const handleUpload = require('../middlewares/handleUpload')
 
 /* GET organizations listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-
+  
 });
 
 router.get('/:id/public', findOrganizationById)
