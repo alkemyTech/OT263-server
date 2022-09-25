0fdbc48449a7fe5b16fb36288cb759eaf719daed
Sebastian Garcia Nallar <sebagn@hotmail.com>
2022-09-25 11:33:05 -0300

    category service refactorized

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,73 +1,57 @@
-const { Categories } = require('../models')
-const createError = require('http-errors')
-const { updateCategorySchema } = require("../util/category.joi");
-const Joi = require('joi')
+const { Categories } = require("../models");
+const createError = require("http-errors");
 
-const getCategories = async (req, res) => {
-	try {
-		const categories = await Categories.findAll()
-		return res.status(200).json(categories)
-	} catch (err) {
-		return res.status(404).json(createError.NotFound(err.message))
-	}
-}
+const getCategories = async (req, res,next) => {
+    try {
+        const categories = await Categories.findAll();
+        return res.status(200).json(categories);
+    } catch (err) {
+        next(err);
+    }
+};
 
-const createCategory = async (req, res) => {
-	try {
-		const { error } = validateCategory(req.body)
-		if (error) return res.status(400).json(createError.BadRequest(error.details[0].message))
-
-		const category = await Categories.create(req.body)
-
-		return res.status(200).json(category)
-	} catch (error) {
-		return res.status(500).json(createError.InternalServerError(error.message))
-	}
-}
-
-function validateCategory(entry) {
-	const schema = Joi.object({
-		name: Joi.string().required(),
-		message: Joi.string().min(2).required()
-	})
-
-	return schema.validate(entry)
-}
+const createCategory = async (req, res, next) => {
+	const { name, description } = req.body;
+    try {
+        const newCategory = await Categories.create({ name, description });
+        return res.status(201).json(newCategory);
+    } catch (err) {
+        next(err);
+    }
+};
 
 const updateCategory = async (req, res, next) => {
-	try {
-		const id = req.params.id;
-		const { name, description } = req.body;
-		await updateCategorySchema.validateAsync({name, description});
-	
-		const updatedCategory = await Categories.update({name, description}, {where: {id}});
-		if (updatedCategory[0] === 0) {
-			throw createError.NotFound("Category not found");
+    const id = req.params.id;
+    const { name, description } = req.body;
+    try {
+        const updatedCategory = await Categories.update(
+            { name, description },
+            { where: { id } }
+        );
+        if (!updatedCategory[0]) {
+			throw createError.NotFound('Category not found');
 		}
+        return res.status(200).json(req.body);
+    } catch (err) {
+        next(err);
+    }
+};
 
-		return res.status(200).json(req.body);
-	} catch (err) {
-		next(err);
-	}
-}
+const deleteCategory = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const row = await Categories.destroy({ where: { id } });
+		if (!row) throw createError.NotFound('Category not found');
 
-
-const deleteCategory = async (req, res) => {
-	const { id } = req.params
-	try {
-		const row = await Categories.destroy({ where: { id } })
-		if (!row) return res.status(404).json(createError.NotFound())
-
-		return res.status(204).send()
-	} catch (error) {
-		return res.status(500).json(createError.InternalServerError())
-	}
-}
-
+        return res.status(204).send();
+    } catch (err) {
+        next(err);
+    }
+};
 
 module.exports = {
-	createCategory,
-	updateCategory,
-	deleteCategory,
-	getCategories
-}
+    createCategory,
+    updateCategory,
+    deleteCategory,
+    getCategories,
+};
--- util/category.joi.js
+++ util/category.joi.js
@@ -1,8 +1,21 @@
-const Joi = require('joi');
+const Joi = require("joi");
+
+const createCategorySchema = Joi.object({
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+});
 
 const updateCategorySchema = Joi.object({
     name: Joi.string(),
     description: Joi.string(),
 });
 
-module.exports = { updateCategorySchema };
+const findCategorySchema = Joi.object({
+    id: Joi.number().integer().required(),
+});
+
+module.exports = {
+    createCategorySchema,
+    updateCategorySchema,
+    findCategorySchema,
+};
